3f79452a0605d0e501fde8ff768186c8492c4cdc
Mario <mario.1698.mg@gmail.com>
2025-06-15 11:06:40 -0600

    dominancia

--- arrays/5ArrayOfMultiples.js
+++ arrays/5ArrayOfMultiples.js
@@ -1,6 +1,6 @@
 /**
  * 
- * 
+ *
 Array of Multiples
  
 
@@ -13,3 +13,15 @@ Examples:
  * 
  * 
  */
+
+
+function multiplosArr(num, long) {// creamos la funcion y asignamos los parametros num(numero) y long(longitud) 
+    let resultado = []; //se crea una variable y se asigna a un array vacio
+    for (i = 1; i <= long; i++) { //se crea un ciclo for para hacer el recorrido de 0 hasta el valor de long y con un aumento de 1
+        resultado.push(num * i); // se agregan os elementos al nuevo array con push y se hace la operacion de num * el valor de i
+    }
+    return resultado; //se devuelve el resultado en un array nuevo
+}
+
+console.log(multiplosArr(2, 10)); //se muestra los resultados 
+console.log(multiplosArr(17, 6)); //se mjestran resultados
--- arrays/6Positive_dominance_Array.js
+++ arrays/6Positive_dominance_Array.js
@@ -16,3 +16,24 @@ Example:
  * 
  * 
  */
+function arrayPositivo(dominancia) { //Se declara la funcion arrayPositivo
+    let dominanciaPositiva = 0;     //se declaran las variables 
+    let dominanciaNegativa = 0;
+
+    for (i = 0; i < dominancia.length; i++) { //se crea un ciclo for, para recorrer el array dominancia
+
+        if (dominancia[i] > 0){             //se crea la condicion si el valor del array es mayor que 0 
+             dominanciaPositiva++;             //se incrementa el recorrido en 1 en la variable dominancia positiva y se guarda
+
+         } else if (dominancia[i] < 0)          //si no si dominancia es menor que 0 
+            {
+                dominanciaNegativa++;            //se incrementa el recorrido en 1 en la variable dominancia Negativa y se guarda
+
+            }
+    }
+
+    return dominanciaPositiva > dominanciaNegativa;   //se hace una comparacion si la dominanciaPositiva es mayor se devulve un true(positivo) si no un false
+}
+
+console.log(arrayPositivo([-1, -3, -5, 4, 6767])); 
+console.log(arrayPositivo([-1, 3, -5, 4, 2])); 
